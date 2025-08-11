64393a38b829f41c35c4221933c8d621afea4bb1
Rizzy-2005 <ririshikesh2005@gmail.com>
2025-08-12 00:59:15 +0530

    just added login and while running it is displayed.

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const connectDb = require("./config/db");
 //Initializing the express app
 const app = express();
 
-//connect to mongodb
+//Connect to mongodb
 connectDb();
 
+//Using different routes and static pages
+app.use(express.static(path.join(__dirname, "./public")));
+
 const PORT = 3000;
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname,"./views/login.html"));
+});
 
 app.listen(PORT, (err) => {
   if(err) throw err;
