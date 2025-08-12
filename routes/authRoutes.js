a60b27e289ff905490e667caadd04ae1e6c2c9a4
Irene-sketch <irenetreessraj@gmail.com>
2025-08-12 12:26:55 +0530

    Finally after type issue it got worked. Login auth ready

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,61 +1,9 @@
-const Teacher = require('./models/teacherModel');
+//Importing the modules and controller
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
 
+//Rerouting to controller.
+router.post('/', authController.login);
 
-function validateLoginInput(req, res, next) {
-    const { username, password } = req.body;
-
-    if (!username?.trim() || !password) {
-        return res.status(400).json({
-            success: false,
-            message: 'Username and password are required'
-        });
-    }
-
-    req.body.username = username.trim();
-    next();
-}
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
-});
-
-app.post('/login', validateLoginInput, async (req, res, next) => {
-    const { username, password } = req.body;
-
-    try {
-        const teacher = await Teacher.findOne({ username });
-
-        if (teacher && teacher.password === password) {
-            res.json({
-                success: true,
-                message: 'User Successfully Logged in',
-                teacher: {
-                    id: teacher._id,
-                    username: teacher.username,
-                    firstName: teacher.firstName,
-                    lastName: teacher.lastName,
-                    subject: teacher.subject
-                }
-            });
-        } else {
-            res.status(401).json({
-                success: false,
-                message: 'Invalid Credentials'
-            });
-        }
-    } catch (error) {
-        next(error);
-    }
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        success: false,
-        message: 'Something went wrong!'
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = router;
