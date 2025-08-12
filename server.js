4f6267866aee690042e22ba55fb15cb1bc7dc71b
raihaneyyy <141394375+Raihan005@users.noreply.github.com>
2025-08-12 09:09:57 +0530

    Updated add teacher page

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const PORT = 3000;
 app.use(express.static(__dirname))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));//loading the default page
 });
+
+app.post('/add-teacher',(req,res)=>{ //POST req from frontend
+    const receivedData = req.body;
+    console.log('Recieved Data: ',receivedData);
+    res.json({ message: 'Success' });   //sending back response
+})
 
 app.listen(PORT,()=>{
     console.log(`Server running at http://localhost:${PORT}`);
-})
-
-app.post('/add-teacher',(req,res)=>{
-    const receivedData = req.body;
-    console.log('Recieved Data: ',receivedData);
-    res.json({ message: 'Success' });
 })
