ff95773943d6517842e145fbda717471a61bb43a
Rizzy-2005 <ririshikesh2005@gmail.com>
2025-10-04 07:32:31 +0530

    added an extra request controller and route

---
+++ controllers/requestController.js
@@ -0,0 +1,9 @@
+const requests = require('../models/requestModel')
+
+exports.sendRequest = async (req,res)=>{}
+
+exports.receiveRequest = async (req,res)=>{}
+
+exports.acceptReq = async (req,res)=>{}
+
+exports.declineReq = async (req,res)=>{}
