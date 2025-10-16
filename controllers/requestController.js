7c0e7c9a27835322632071f83ab39abcebf22313
Ajzal <ajzalasain123@gmail.com>
2025-10-16 21:55:09 +0530

    Added few functions related to requests. mentioned necessary fields in examAllocations.html

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -1,9 +1,43 @@
 const requests = require('../models/requestModel')
 
-exports.sendRequest = async (req,res)=>{}
+exports.sendRequest = async (req, res) => {
+  try {
+    const { scheduleId, fromTeacherId, toTeacherId, message,examId } = req.body;
 
-exports.receiveRequest = async (req,res)=>{}
+    const newRequest = new requests({
+        examId: examId,
+        allocationId: scheduleId,  
+        fromTeacherId,
+        toTeacherId,
+        reason: message,
+    });
 
-exports.acceptReq = async (req,res)=>{}
+    const savedRequest = await newRequest.save();
+    res.status(201).json({ success: true, request: savedRequest });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-exports.declineReq = async (req,res)=>{}
+exports.receiveRequest = async (req,res)=>{
+    try{
+        const msgs = await requests.find({toTeacherId:req.params.id});
+        res.status(200).send({success:true, requestList:msgs});
+    }catch(error){
+        res.status(400).send({success:false, msg:error.message})
+    }
+}
+
+exports.updateRequestStatus = async (req, res) => {
+  const id = req.params.id;
+  const stat = req.body.status;
+  try {
+    const updatedRequest = await requests.findByIdAndUpdate( id,{ $set: { status: stat } }, { new: true });
+    if (!updatedRequest) {
+      return res.status(404).send({ success: false, msg: "Request not found" });
+    }
+    res.status(200).send({ success: true, request: updatedRequest });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
