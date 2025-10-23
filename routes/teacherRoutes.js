a8fab6f24c3af3addc596ebd5b9f49bd510de855
Ajzal <ajzalasain123@gmail.com>
2025-10-23 18:49:32 +0530

    Fixed the user icon

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const teacherController = require('../controllers/teacherController')
 
-
+router.get('/getUserInfo',teacherController.getInfo)
 router.post('/getSchedule', teacherController.getSchedule);
 router.post('/getAllocationDetails', teacherController.getAllocationDetails);
 
