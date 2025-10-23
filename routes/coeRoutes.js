e9b384ddfb8d0815ebc5b1b3012e3810ef69e1d8
Rizzy-2005 <ririshikesh2005@gmail.com>
2025-10-24 00:02:49 +0530

    Updated Allocations Table

--- routes/coeRoutes.js
+++ routes/coeRoutes.js
@@ -2,12 +2,21 @@
 const express = require("express");
 const router = express.Router();
 const coeController = require("../controllers/coeController");
+const path = require('path');
 
 //Rerouting to controller.
 router.post('/addTeacher', coeController.addTeacher);
 router.get('/getClassrooms',coeController.loadClassrooms);
 router.get('/getTeachers', coeController.getTeachers);
 router.post('/createExam', coeController.createExam);
+router.get('/getExams',coeController.getExams);
+router.get('/allocations/:id', (req, res) => {
+console.log('[ROUTE HIT] /allocations/:id → ID:', req.params.id);
+  res.sendFile(path.join(__dirname, '../views/viewAllocation.html'));
+});
+router.get('/getAllocations/:examId', coeController.getAllocations);
+router.get('/getExam/:id', coeController.getExamById);
+
 
 
 module.exports = router;
